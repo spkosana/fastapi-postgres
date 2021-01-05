32b0fa97063586e819b6be160fd0ede8a6adf31c
spkosana <spkosana.82@gmail.com>
2021-01-05 17:37:21 -0500

    Modifed the message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Form
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -23,3 +23,8 @@ async def home():
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
+
+
+@app.post("/login/")
+async def login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username}
